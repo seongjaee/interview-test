8efa30f446be0ea5477d01ffdb5fd7d8e6afc43e
seongjaee <labyrinthird@gmail.com>
2022-07-25 01:27:38 +0900

    Feat: createPage POST요청 추가

--- src/index.ts
+++ src/index.ts
@@ -19,27 +19,56 @@ export interface Env {
   // MY_BUCKET: R2Bucket;
 }
 
-const url = `https://api.notion.com/v1/databases/${DATABASE_ID}/query`;
+const url = `https://api.notion.com/v1`;
+const headers = {
+  Authorization: `Bearer ${NOTION_API_KEY}`,
+  "Notion-Version": "2022-02-22",
+  "content-type": "application/json;charset=UTF-8",
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET,HEAD,POST,init",
+};
+
 async function gatherResponse(response: Response) {
-  const data = await response.json();
   return JSON.stringify(await response.json());
 }
 
 async function handleRequest() {
   const init = {
     method: "POST",
-    headers: {
-      Authorization: `Bearer ${NOTION_API_KEY}`,
-      "Notion-Version": "2022-02-22",
-      "content-type": "application/json;charset=UTF-8",
-      "Access-Control-Allow-Origin": "*",
-    },
+    headers,
   };
-  const response = await fetch(url, init);
+  const response = await fetch(url + `/databases/${DATABASE_ID}/query`, init);
   const results = await gatherResponse(response);
   return new Response(results, init);
 }
 
+async function handlePostRequest(request: Request) {
+  const req = await request.json();
+  const data = {
+    parent: { database_id: DATABASE_ID },
+    properties: req,
+  };
+  const init = {
+    method: "POST",
+    headers,
+    body: JSON.stringify(data),
+  };
+  const response = await fetch(url + `/pages`, init);
+  if (response.status === 200) {
+    return new Response(`Success`);
+  } else {
+    return new Response(`Failed`);
+  }
+}
+
 addEventListener("fetch", (event) => {
+  const { request } = event;
+  console.log(request);
+
+  if (request.method === "POST") {
+    return event.respondWith(handlePostRequest(request));
+  } else if (request.method === "GET") {
+    return event.respondWith(handleRequest());
+  }
   return event.respondWith(handleRequest());
 });
