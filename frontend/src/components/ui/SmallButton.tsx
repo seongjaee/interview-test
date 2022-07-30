4720fb8e1f26aa67eb9c031823ae1adda4354574
seongjaee <labyrinthird@gmail.com>
2022-07-30 23:06:23 +0900

    Feat: 작은 버튼 컴포넌트

---
+++ frontend/src/components/ui/SmallButton.tsx
@@ -0,0 +1,52 @@
+import React, { ReactNode } from "react";
+import styled from "styled-components";
+
+const StyledButton = styled.div<{
+  type: "primary" | "secondary";
+  width: string;
+}>`
+  display: flex;
+  gap: 0.6rem;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  line-height: 3rem;
+  width: ${(props) => props.width};
+  height: 2rem;
+  border: 1px solid #2f3437;
+  border-radius: 3px;
+  color: ${(props) => (props.type === "primary" ? "#f5f4f1" : "#2f3437")};
+  user-select: none;
+  cursor: pointer;
+  font-size: 1rem;
+  background-color: ${(props) =>
+    props.type === "primary" ? "#2f3437" : "none"};
+  :hover {
+    background-color: ${(props) =>
+      props.type === "primary" ? "#161819" : "#deded9"};
+  }
+`;
+
+interface ButtonProps {
+  label?: string;
+  type?: "primary" | "secondary";
+  width?: string;
+  onClick: () => void;
+  children?: ReactNode;
+}
+
+function SmallButton({
+  label,
+  width = "6rem",
+  type = "primary",
+  onClick,
+  children,
+}: ButtonProps) {
+  return (
+    <StyledButton onClick={onClick} type={type} width={width}>
+      {children} {label}
+    </StyledButton>
+  );
+}
+
+export default SmallButton;
